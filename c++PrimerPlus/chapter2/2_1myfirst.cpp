d78ce75eb712727ca2886727c5b924088821548e
jack_shi <carl3633524461@outlook.com>
2023-12-30 12:18:05 +0800

    首次提交

---
+++ c++PrimerPlus/chapter2/2_1myfirst.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+int main() {
+
+    using namespace std;
+
+    cout << "Hi there, I'm stoner from changzhi, shanxi, China";
+    cout << endl;
+    cout << "You won`t regret it !";
+    return 0;
+}
